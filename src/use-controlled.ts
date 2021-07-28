dde5f2a04340cc71c10269b7373f771d7629813d
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-07-28 04:04:29 -0400

    Fix type

--- src/use-controlled.ts
+++ src/use-controlled.ts
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 export type UseControlledProps<T = unknown> = {
-  controlled: T | undefined;
-  default: T | undefined;
+  controlled?: T;
+  default?: T;
 };
 
 export function useControlled<T = unknown>(props: UseControlledProps<T>) {
   const ref = React.useRef(props.controlled !== undefined);
-  const [valueState, setValue] = React.useState<T>(props.default);
+  const [valueState, setValue] = React.useState<T | undefined>(props.default);
   const value = ref.current ? props.controlled : valueState;
 
   const setValueIfUncontrolled: React.Dispatch<T> = React.useCallback((newValue) => {
